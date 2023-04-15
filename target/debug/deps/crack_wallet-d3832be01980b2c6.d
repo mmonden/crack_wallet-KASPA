ee2fea92b85f8466ec2d6f86f751530619c717e4
mmonden <matt.mll@hotmail.com>
2023-04-15 23:39:08 +0200

    test

---
+++ target/debug/deps/crack_wallet-d3832be01980b2c6.d
@@ -0,0 +1,6 @@
+/home/matthiasmonden/projects/rusty-kaspa/crack-wallet/target/debug/deps/crack_wallet-d3832be01980b2c6: src/main.rs src/word_combinations.rs
+
+/home/matthiasmonden/projects/rusty-kaspa/crack-wallet/target/debug/deps/crack_wallet-d3832be01980b2c6.d: src/main.rs src/word_combinations.rs
+
+src/main.rs:
+src/word_combinations.rs:
---
+++ target/debug/deps/either-398422a02246f2e4.d
@@ -0,0 +1,7 @@
+/home/matthiasmonden/projects/rusty-kaspa/crack-wallet/target/debug/deps/either-398422a02246f2e4.rmeta: /home/matthiasmonden/.cargo/registry/src/github.com-1ecc6299db9ec823/either-1.8.1/src/lib.rs
+
+/home/matthiasmonden/projects/rusty-kaspa/crack-wallet/target/debug/deps/libeither-398422a02246f2e4.rlib: /home/matthiasmonden/.cargo/registry/src/github.com-1ecc6299db9ec823/either-1.8.1/src/lib.rs
+
+/home/matthiasmonden/projects/rusty-kaspa/crack-wallet/target/debug/deps/either-398422a02246f2e4.d: /home/matthiasmonden/.cargo/registry/src/github.com-1ecc6299db9ec823/either-1.8.1/src/lib.rs
+
+/home/matthiasmonden/.cargo/registry/src/github.com-1ecc6299db9ec823/either-1.8.1/src/lib.rs:
